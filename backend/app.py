04aee1d37560ae5edd9368145912cdedf751fd67
Farahad Khurami <fkhurami98@gmail.com>
2025-07-27 17:11:45 +0100

    Fix issues with json response being returned

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 import base64
-import uvicorn
+import json
 
 from openai import OpenAI, BadRequestError
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 
-from config import API_KEY, SYSTEM_PROMPT, USER_PROMPT
+from config import API_KEY, SYSTEM_PROMPT, USER_PROMPT, MODEL
 
 app = FastAPI()
 
@@ -16,40 +16,38 @@ client = OpenAI(api_key=API_KEY)
 @app.post("/detect-food")
 async def detect_food(image: UploadFile = File(...)):
 
-    image_bytes = await image.read()
-    image_base64 = base64.b64encode(image_bytes).decode("utf-8")
+    img_bytes = await image.read()
+    img_b64 = base64.b64encode(img_bytes).decode("utf-8")
 
-    try:
-        response = client.chat.completions.create(
-            model="gpt-4.1",
-            messages=[
-                {"role": "system", "content": SYSTEM_PROMPT},
+    msg = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": USER_PROMPT},
                 {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": USER_PROMPT,
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/jpeg;base64,{image_base64}"
-                            },
-                        },
-                    ],
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{img_b64}"},
                 },
             ],
+        },
+    ]
+
+    try:
+        res = client.chat.completions.create(
+            model=MODEL,
+            messages=msg,
+            response_format={"type": "json_object"},
         )
-        food_items = response.choices[0].message.content
-        return JSONResponse(content={"food_items": food_items})
-    except BadRequestError as e:
-        e = str(e)
-        if "unsupported image" in e or "invalid_image_format" in e:
+        res_obj = json.loads(res.choices[0].message.content)
+        return JSONResponse(content=res_obj)
+    except BadRequestError as err:
+        err_str = str(err)
+        if "unsupported image" in err_str or "invalid_image_format" in err_str:
             msg = (
                 f"The file you uploaded is not supported. "
                 f"Supported image file formats are: png, jpeg, gif, webp. "
                 f"Your file type: {image.content_type}"
             )
             return JSONResponse(content={"error": msg}, status_code=400)
-        return JSONResponse(content={"error": e}, status_code=400)
+        return JSONResponse(content={"error": err_str}, status_code=400)
--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+MODEL = "gpt-4o-mini"
 
 API_KEY = os.getenv("OPENAI_API_KEY")
 
